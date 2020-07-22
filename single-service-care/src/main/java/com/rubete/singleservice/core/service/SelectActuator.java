dde04ba4432c80d16a6b9f9f1bd4cbee0dc950ab
shenglong <87049333@qq.com>
2020-07-22 14:03:02 +0800

    修改

--- single-service-care/src/main/java/com/rubete/singleservice/core/service/SelectActuator.java
+++ single-service-care/src/main/java/com/rubete/singleservice/core/service/SelectActuator.java
@@ -8,7 +8,6 @@ import com.rubete.singleservice.core.execute.IActuator;
 public class SelectActuator extends AbsServiceActuator {
     @Override
     public Object action(IActuator actuator) {
-
         BasicParameterActuator basi = (BasicParameterActuator)actuator;
         return this.getSqlSessionTemplate().selectList(basi.getId(),basi.getObj());
     }
